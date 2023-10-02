02e7f65f59a8013c7bdb53506758ad2d2d67c326
Godwin Opara <godwinopara62@yahoo.com>
2023-10-02 21:24:32 +0100

    added header nav

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Logo from "../images/ngologo.svg";
+
+const Header = () => {
+	return (
+		<header>
+			<nav className="flex justify-between items-center max-w-[1400px] mx-auto">
+				<div className="">
+					<img src={Logo} alt="" className="w-52" />
+				</div>
+
+				<ul className="flex justify-between items-center">
+					<li className="p-5 border-r-2 text-md font-semibold cursor-pointer">Why Health</li>
+					<li className="p-5 border-r-2 text-md font-semibold cursor-pointer">What We Do</li>
+					<li className="p-5 border-r-2 text-md font-semibold cursor-pointer">Where We Work</li>
+					<li className="p-5 border-r-2 text-md font-semibold cursor-pointer">Take Action</li>
+					<li className="p-5 border-r-2 text-md font-semibold cursor-pointer">Crisis Alert</li>
+					<button className=" bg-slate-900 text-white font-bold text-md rounded-sm px-8 py-5">
+						Donate Now
+					</button>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
